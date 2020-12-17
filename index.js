5d37c0d68044a4e1ae66b064bb6d184734abc615
Rodrigo Nunes <69399583+rodrigonuness@users.noreply.github.com>
2020-12-17 13:50:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const os = require('os-utils');
-
 const diskspace = require('diskspace');
 
 os.cpuUsage(function(cpu){
@@ -7,7 +6,7 @@ os.cpuUsage(function(cpu){
 });
 
 diskspace.check('C', function (err, result) {
-    var free_space = (result.free / 1000000000).toFixed(2)
-    var total_space = (result.total / 1000000000).toFixed(2)
-    console.log(`Free space: ${free_space} GB.\nTotal space: ${total_space} GB`)
+    var free_space = (result.free / 1000000000).toFixed(2);
+    var total_space = (result.total / 1000000000).toFixed(2);
+    console.log(`Free space: ${free_space} GB.\nTotal space: ${total_space} GB`);
 });
